69fcd464a933383bd7435cfeaba264b9b511cf3a
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-01-13 22:02:02 +0545

    rails action text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description ])
     end
 end
