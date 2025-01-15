87dd45d546ac0586d374adab40dc5dcafe76d51c
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-01-15 10:46:17 +0545

    dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create email_address: "user@example.com", password: "P@ssword12", password_confirmation: "P@ssword12"
